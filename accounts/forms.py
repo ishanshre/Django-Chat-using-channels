513c447e91a548d1ead53026239b35b5a1302d81
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-01 12:40:24 +0545

    feat: add signup and login views, urls and forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 User = get_user_model()
 
@@ -14,3 +16,13 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','username','email','gender','date_of_birth','facebook','twitter']
+
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput, max_length=255, required=True)
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+    remember_me = forms.BooleanField(required=False, initial=False)
+    
+    class Meta:
+        model = User
+        fields = ["username","password","remember_me"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from accounts.forms import CustomLoginForm, CustomUserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "accounts/signup.html"
+    success_message: str = "User successfully created"
+class UserLoginView(SuccessMessageMixin, LoginView):
+    form_class = CustomLoginForm
+    success_url = reverse_lazy("core:index")
+    template_name: str = "accounts/login.html"
+    success_message: str = "Login Successfull"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("core:index")
+        return super(UserLoginView, self).dispatch(request, *args, **kwargs)
