a2b47992dad12b5ac0a2020a3fee7fb3994e6795
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-01-02 20:15:53 +0545

    feat: style chat list page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def ChatListView(request):
-    return render(request, "index.html")
+    profile = request.user.profile
+    friends = profile.friends.all()
+    context = {
+        'profile':profile,
+        'friends':friends,
+    }
+    return render(request, "index.html", context)
 
 
 def ChatDetailView(request):
